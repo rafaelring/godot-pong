4cb448f2f8e1bb3946cac4702de5c11a6693e8b3
Maurício Verardo <mauricio.verardo@gmail.com>
2021-01-10 21:45:03 -0300

    Player is now a scene and add player 2

--- Scripts/P1_Kinematic.cs
+++ Scripts/P1_Kinematic.cs
@@ -8,19 +8,24 @@ public class P1_Kinematic : KinematicBody2D
 	// private string b = "text";
 
 	[Export] public int speed = 600;
-
+	[Export] public String key_up = "p1_move_up";
+	[Export] public String key_down = "p1_move_down";
 	public Vector2 velocity = new Vector2();
+
+	public override void _Ready()
+	{
+		base._Ready();
+	}
 
 	public override void _PhysicsProcess(float delta)
 	{
 		velocity = new Vector2();
 
-		if (Input.IsActionPressed("p1_move_up"))
+		if (Input.IsActionPressed(this.key_up))
 			velocity.y -= 1;
 
-		if (Input.IsActionPressed("p1_move_down"))
+		if (Input.IsActionPressed(this.key_down))
 			velocity.y += 1;
-
 
 		MoveAndSlide(velocity.Normalized() * speed);
 	}
